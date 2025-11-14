572f11374d101e494e481b433fb3674186fc3660
bacha <aymene98tech@gmail.com>
2025-11-14 19:29:01 +0100

    add pitch and yaw code

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ char receivedChar;
 MPU6050 mpu;
 Servo esc5;
 Servo esc6;
+Servo esc3;
+Servo esc9;
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -41,15 +43,25 @@ float Kp = 1.0;
 float Ki = 3;
 float Kd = 1.0;
 // متغيرات PID
-float integral = 0.0;
-float derivative = 0.0;
+float rollIntegral = 0.0; 
+float pitchIntegral = 0.0;
+float yawIntegral = 0.0;
+float rollderivative = 0.0;
+float pitchDerivative = 0.0;
+float yawDerivative = 0.0;
 float controlOutput5 = 0.0;
+float controlOutput6 = 0.0;
+float controlOutput3 = 0.0;
+float controlOutput9 = 0.0;
 float derivativeMax = 500.0;
 float integralMax = 200.0;
-float controlOutput6 = 0.0;
-float previousError = 0.0;
+float previousRollError = 0.0;
+float previousPitchError = 0.0;
+float previousYawError = 0.0;
 float setpoint = 0.0;
-float currentAngle = 0.0;
+float currentRollAngle = 0.0;
+float currentPitchAngle = 0.0;
+float currentYawAngle = 0.0;
 float outputMax = 1900.0;
 float outputMin = 1010.0;
 // متغيرات الوقت
@@ -61,9 +73,12 @@ void setup()
 {
   esc5.attach(5, 1000, 2000);
   esc6.attach(6, 1000, 2000);
+  esc9.attach(9, 1000, 2000);
+  esc3.attach(3, 1000, 2000);
   esc5.writeMicroseconds(2000);
   esc6.writeMicroseconds(2000);
-
+  esc9.writeMicroseconds(2000);
+  esc3.writeMicroseconds(2000);
   bluetooth.begin(9600);
   Wire.begin();
   TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
@@ -119,6 +134,8 @@ void setup()
   delay(4000);
   esc5.writeMicroseconds(1000);
   esc6.writeMicroseconds(1000);
+  esc9.writeMicroseconds(1000);
+  esc3.writeMicroseconds(1000);
   Serial.println("low");
   delay(6000);
 
@@ -190,31 +207,63 @@ void loop()
     // Serial.print("\t");
   }
   
-  currentAngle = ypr[ROLL] * (180 / M_PI);
-  float error = setpoint - currentAngle;
+  currentRollAngle = ypr[ROLL] * (180 / M_PI);
+  currentPitchAngle = ypr[PITCH] * (180 / M_PI);
+  currentYawAngle = ypr[YAW] * (180 / M_PI);
+  float rollError = setpoint - currentRollAngle;
+  float pitchError = setpoint - currentPitchAngle;
+  float yawError = setpoint - currentYawAngle;
   currentTime = millis();
   deltaT = (currentTime - previousTime) / 1000.0; // تحويل إلى ثوانٍ
   previousTime = currentTime;
-  integral = integral + (error * deltaT);
+  rollIntegral = rollIntegral + (rollError * deltaT);
+  pitchIntegral = pitchIntegral + (pitchError * deltaT);
+  yawIntegral = yawIntegral + (yawError * deltaT);
   // قيد التكامل ليكون ضمن الحدود
-  if (integral > integralMax) integral = integralMax;
-  if (integral < -integralMax) integral = -integralMax;
-  derivative = (error - previousError) / deltaT;
-  previousError = error;
-  // قيد المشتق ليكون ضمن الحدود
-  if (derivative > derivativeMax) derivative = derivativeMax;
-  if (derivative < -derivativeMax) derivative = -derivativeMax;
+  if (rollIntegral > integralMax) rollIntegral = integralMax;
+  if (rollIntegral < -integralMax) rollIntegral = -integralMax;
 
-  controlOutput5 = 1300 + (Kp * error + Ki * integral + Kd * derivative);
-  controlOutput6 = 1300 - (Kp * error + Ki * integral + Kd * derivative);
+  if (pitchIntegral > integralMax) pitchIntegral = integralMax;
+  if (pitchIntegral < -integralMax) pitchIntegral = -integralMax;
+
+  if (yawIntegral > integralMax) yawIntegral = integralMax;
+  if (yawIntegral < -integralMax) yawIntegral = -integralMax;
+
+  pitchDerivative = (pitchError - previousPitchError) / deltaT;
+  previousPitchError = pitchError;
+  // قيد المشتق ليكون ضمن الحدود
+  if (pitchDerivative > derivativeMax) pitchDerivative = derivativeMax;
+  if (pitchDerivative < -derivativeMax) pitchDerivative = -derivativeMax;
+
+  rollderivative = (rollError - previousRollError) / deltaT;
+  previousRollError = rollError;  
+  // قيد المشتق ليكون ضمن الحدود
+  if (rollderivative > derivativeMax) rollderivative = derivativeMax;
+  if (rollderivative < -derivativeMax) rollderivative = -derivativeMax;
+  yawDerivative = (yawError - previousYawError) / deltaT;
+  previousYawError = yawError;
+  // قيد المشتق ليكون ضمن الحدود
+  if (yawDerivative > derivativeMax) yawDerivative = derivativeMax;
+  if (yawDerivative < -derivativeMax) yawDerivative = -derivativeMax;
+
+  controlOutput5 = 1300 + (Kp * rollError + Ki * rollIntegral + Kd * rollderivative);
+  controlOutput6 = 1300 - (Kp * rollError + Ki * rollIntegral + Kd * rollderivative);
+  controlOutput3 = 1300 + (Kp * pitchError + Ki * pitchIntegral + Kd * pitchDerivative);
+  controlOutput9 = 1300 - (Kp * pitchError + Ki * pitchIntegral + Kd * pitchDerivative);
 
   if (controlOutput5 > outputMax) controlOutput5 = outputMax;
   if (controlOutput5 < outputMin) controlOutput5 = outputMin;
   if (controlOutput6 > outputMax) controlOutput6 = outputMax;
   if (controlOutput6 < outputMin) controlOutput6 = outputMin;
+  if (controlOutput3 > outputMax) controlOutput3 = outputMax;
+  if (controlOutput3 < outputMin) controlOutput3 = outputMin;
+  if (controlOutput9 > outputMax) controlOutput9 = outputMax;
+  if (controlOutput9 < outputMin) controlOutput9 = outputMin;
 
   esc5.writeMicroseconds(controlOutput5);
   esc6.writeMicroseconds(controlOutput6);
+  esc3.writeMicroseconds(controlOutput3);
+  esc9.writeMicroseconds(controlOutput9);
 
   if (bluetooth.available())
   {
@@ -240,15 +289,15 @@ void loop()
   // Serial.print("Error: ");
   // Serial.print(error);
   // Serial.print("    ");
-  // Serial.print("Integral: ");
-  // Serial.print(Ki*integral);
+  // Serial.print("rollIntegral: ");
+  // Serial.print(Ki*rollIntegral);
   // Serial.print("    ");
-  // Serial.print("Derivative: "); 
-  // Serial.println(Kd*derivative);
+  // Serial.print("rollderivative: "); 
+  // Serial.println(Kd*rollderivative);
   // // Serial.print("    ");
-  // // Serial.print("Integral: ");
-  // // Serial.print(integral);
+  // // Serial.print("rollIntegral: ");
+  // // Serial.print(rollIntegral);
   // // Serial.print("    ");
-  // // Serial.print("Derivative: ");
-  // // Serial.println(derivative);
+  // // Serial.print("rollderivative: ");
+  // // Serial.println(rollderivative);
 }
